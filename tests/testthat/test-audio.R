aabff1dbf11b81bd6c8f741d7026445398bc92cd
Shota Ochi <shotaochi1990@gmail.com>
2023-07-18 18:37:55 +0900

    make test more flexible

--- tests/testthat/test-audio.R
+++ tests/testthat/test-audio.R
@@ -4,10 +4,23 @@ test_that("audio",
   expect_no_error(getAudioDevices())
   dev <- getAudioDevices()
   skip_if(length(dev) < 1, "A list of audio devices is not available.")
-#  tryCatch(SDLaudio$new(sound), error = function(e) skip("failed to open audio device."))
   expect_no_error(SDLaudio$new(sound))
-  expect_no_error(SDLaudio$new(sound, 0))
-  expect_no_error(SDLaudio$new(sound, dev[1]))
+  num_dev = -1
+  for (i in 0:(length(dev) - 1))
+  {
+    tmp <- try(SDLaudio$new(sound, i), silent = TRUE)
+    if (class(tmp)[1] != "try-error")
+    {
+      num_dev <- i
+      break;
+    }
+  }
+  if (num_dev == -1)
+  {
+    skip("failed to open audio device.")
+  }
+  expect_no_error(SDLaudio$new(sound, num_dev))
+  expect_no_error(SDLaudio$new(sound, dev[num_dev + 1]))
   expect_error(SDLaudio$new(sound, TRUE))
   expect_error(SDLaudio$new(sound, -1))
   expect_error(SDLaudio$new(sound, "i believe no device name is like this."))
